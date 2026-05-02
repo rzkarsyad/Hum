916d9f32a87663129d3503aa2e7fddb0be7fc2d4
Rizki Arsyad <rzkarsyad@icloud.com>
2026-05-03 02:12:28 +0700

    feat: uniform title3 font size and isManuallyHidden in LyricsState

--- Hum/LyricsEngine/LyricsState.swift
+++ Hum/LyricsEngine/LyricsState.swift
@@ -4,4 +4,5 @@ import Foundation
 final class LyricsState: ObservableObject {
     @Published var lines: [LyricLine] = []
     @Published var syncOffset: TimeInterval = 0
+    @Published var isManuallyHidden: Bool = false
 }
