54b8f11a02b5572f8ac3103aad4630cd37e6f7d0
Rizki Arsyad <rzkarsyad@icloud.com>
2026-05-03 05:17:57 +0700

    fix: VStack offset scroll (no ScrollView), LRCLIB duration matching for better timing

--- Hum/Models/Track.swift
+++ Hum/Models/Track.swift
@@ -1,5 +1,15 @@
+import Foundation
+
 struct Track: Equatable, Hashable {
     let title: String
     let artist: String
     let album: String
+    let duration: TimeInterval?
+
+    init(title: String, artist: String, album: String, duration: TimeInterval? = nil) {
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.duration = duration
+    }
 }
